40515ae6d447945cc5ed20040d154a63afe708d6
Jacinto Mba Cantero <jacmba@gmail.com>
2024-05-22 23:43:48 +0100

    Initial commit

---
+++ src/main/java/net/jazbelt/jazflixboapi/JazflixBoApiApplication.java
@@ -0,0 +1,13 @@
+package net.jazbelt.jazflixboapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazflixBoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazflixBoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/jazbelt/jazflixboapi/JazflixBoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jazbelt.jazflixboapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazflixBoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

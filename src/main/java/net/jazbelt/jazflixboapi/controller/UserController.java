0a23e0ba83231afd181c8cd330c302a9c2d77249
Jacinto Mba Cantero <jacmba@gmail.com>
2024-05-24 15:13:41 +0100

    Updated User endpoint documentation

--- src/main/java/net/jazbelt/jazflixboapi/controller/UserController.java
+++ src/main/java/net/jazbelt/jazflixboapi/controller/UserController.java
@@ -1,5 +1,7 @@
 package net.jazbelt.jazflixboapi.controller;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import net.jazbelt.jazflixboapi.domain.IUserService;
 import net.jazbelt.jazflixboapi.model.entity.User;
@@ -21,29 +23,50 @@ public class UserController {
     }
 
     @GetMapping
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Return list of users")
+    })
     public List<User> getUsers() {
         return service.retrieveAllUsers();
     }
 
     @GetMapping("{id}")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Return single user"),
+            @ApiResponse(responseCode = "400", description = "User not found")
+    })
     public User getUser(@PathVariable("id") String id) {
         return service.retrieveUserDetails(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "User successfully created"),
+            @ApiResponse(responseCode = "400", description = "Validation error")
+    })
     public User postCreateUser(@RequestBody @Valid User user) {
         return service.createUser(user);
     }
 
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
+    @ApiResponses({
+            @ApiResponse(responseCode = "202", description = "User successfully updated"),
+            @ApiResponse(responseCode = "400", description = "Validation error"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "409", description = "Path and object user IDs do not match")
+    })
     public User putUpdateUser(@PathVariable("id") String id, @Valid @RequestBody User user) {
         return service.updateUser(id, user);
     }
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "User successfully deleted"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     public void deleteUser(@PathVariable("id") String id) {
         service.deleteUser(id);
     }
--- src/main/java/net/jazbelt/jazflixboapi/model/entity/User.java
+++ src/main/java/net/jazbelt/jazflixboapi/model/entity/User.java
@@ -1,5 +1,6 @@
 package net.jazbelt.jazflixboapi.model.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -15,16 +16,20 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @ToString
 @Document(collection = "users")
+@Schema(description = "Jazflix user object")
 public class User {
 
     @Id
+    @Schema(description = "Database unique ID", example = "abc123456")
     private String id;
 
     @NotBlank
     @NotNull
     @Email
+    @Schema(description = "User name (OAuth2 Google email)", example = "john.doe@gmail.com")
     private String name;
 
     @NotNull
+    @Schema(description = "Flag to allow user auth", example = "true")
     private Boolean enabled;
 }
